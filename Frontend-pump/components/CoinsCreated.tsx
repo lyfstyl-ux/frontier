b5eb971903ad1934f467dc3203ba46354e82f21f
lyfstyl-ux <michealwritesyes@gmail.com>
2025-08-22 16:09:03 +0100

    Delete Frontend-pump directory

--- Frontend-pump/components/CoinsCreated.tsx
+++
@@ -1,9 +0,0 @@
-import { CryptoPortfolio } from './crypto-portfolio'
-
-const CoinsCreated = ({ heldOnly, createdOnly }: { heldOnly?: boolean, createdOnly?: boolean }) => {
-    return (
-        <CryptoPortfolio heldOnly={heldOnly} createdOnly={createdOnly} />
-    )
-}
-
-export default CoinsCreated
